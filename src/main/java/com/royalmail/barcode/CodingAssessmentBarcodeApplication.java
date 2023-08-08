f46083cd0e395a1c2493bc0da79424dc76b792a8
612750911 <jack.3.hughes@bt.com>
2023-08-08 20:50:14 +0100

    initial commit of project

---
+++ src/main/java/com/royalmail/barcode/CodingAssessmentBarcodeApplication.java
@@ -0,0 +1,13 @@
+package com.royalmail.barcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssessmentBarcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingAssessmentBarcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/royalmail/barcode/CodingAssessmentBarcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royalmail.barcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAssessmentBarcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
