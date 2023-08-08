5cb34f46ef75205995b16b71c669def8f05aa19a
612750911 <jack.3.hughes@bt.com>
2023-08-08 21:21:31 +0100

    adding controller class. Adding Method in Validator for stubbing purposes

--- src/main/java/com/royalmail/barcode/controller/BarcodeValidationController.java
+++ src/main/java/com/royalmail/barcode/controller/BarcodeValidationController.java
@@ -1,4 +1,25 @@
 package com.royalmail.barcode.controller;
 
+import com.royalmail.barcode.utilities.BarcodeValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BarcodeValidationController {
+
+    @Autowired
+    BarcodeValidator validator;
+
+    @GetMapping("/validate/{barcode}")
+    public ResponseEntity<Boolean> validateBarcode(@PathVariable(required = true) String barcode) {
+
+        boolean isValid = validator.validateBarcode(barcode);
+
+        ResponseEntity<Boolean> responseEntity = new ResponseEntity<>(isValid, HttpStatus.OK);
+        return responseEntity;
+    }
 }
--- src/main/java/com/royalmail/barcode/utilities/BarcodeValidator.java
+++ src/main/java/com/royalmail/barcode/utilities/BarcodeValidator.java
@@ -1,4 +1,8 @@
 package com.royalmail.barcode.utilities;
 
 public class BarcodeValidator {
+
+    public boolean validateBarcode(String barcode){
+        return false;
+    }
 }
