814e47f6f3a23ed0b75877f80f5df77fdb975572
612750911 <jack.3.hughes@bt.com>
2023-08-08 21:29:00 +0100

    Adding test scenarios with expectations. TDD beginning

--- src/test/java/com/royalmail/barcode/utilities/BarcodeValidatorTest.java
+++ src/test/java/com/royalmail/barcode/utilities/BarcodeValidatorTest.java
@@ -1,4 +1,66 @@
 package com.royalmail.barcode.utilities;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 public class BarcodeValidatorTest {
+
+    @Autowired
+    BarcodeValidator barcodeValidator;
+
+    /**
+     * Testing scenario where valid barcode is provided
+     * Expected:
+     */
+    @Test
+    public void testValidBarcode() {
+
+    }
+
+    /**
+     * Testing scenario where invalid barcode is provided - incorrect length
+     * Expected:
+     */
+    @Test
+    public void testInvalidBarcode_Length() {
+
+    }
+
+    /**
+     * Testing scenario where invalid barcode is provided - Invalid prefix
+     * Expected:
+     */
+    @Test
+    public void testInvalidBarcode_Prefix() {
+
+    }
+
+    /**
+     * Testing scenario where invalid barcode is provided - non=digit character provided
+     * Expected:
+     */
+    @Test
+    public void testInvalidBarcode_SerialNumber() {
+
+    }
+
+    /**
+     * Testing scenario where invalid barcode is provided - Invalid CheckDigit
+     * Expected:
+     */
+    @Test
+    public void testInvalidBarcode_CheckDigit() {
+
+    }
+
+    /**
+     * Testing scenario where invalid barcode is provided - Invalid CountryCode
+     * Expected:
+     */
+    @Test
+    public void testInvalidBarcode_CountryCode() {
+
+    }
 }
