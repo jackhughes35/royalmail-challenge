cb4f282cc03ec1d3f8f14c660230cf83fb33549e
612750911 <jack.3.hughes@bt.com>
2023-08-08 21:24:45 +0100

    Adding test scenarios with expectations. TDD beginning

--- src/test/java/com/royalmail/barcode/controller/BarcodeValidationControllerTest.java
+++ src/test/java/com/royalmail/barcode/controller/BarcodeValidationControllerTest.java
@@ -1,4 +1,65 @@
 package com.royalmail.barcode.controller;
 
 public class BarcodeValidationControllerTest {
+
+    /**
+     * Testing scenario where incorrect endpoint is hit
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where endpoint is hit with incorrect HTTP method
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where mandatory path parameter is not present
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where request parameter is empty
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where request parameter is null
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where validator returns true
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where validator returns false
+     * Expected:
+     */
+
+    /**
+     * Testing scenario where validator throws {@link com.royalmail.barcode.exception.BarcodeValidationException}
+     * Expected: HTTP 400
+     */
+
+    /**
+     * Testing scenario where validator throws {@link com.royalmail.barcode.exception.InvalidCountryCodeException}
+     * Expected: HTTP 400
+     */
+
+    /**
+     * Testing scenario where validator throws {@link com.royalmail.barcode.exception.InvalidCheckDigitException}
+     * Expected: HTTP 400
+     */
+
+    /**
+     * Testing scenario where validator throws {@link com.royalmail.barcode.exception.InvalidPrefixException}
+     * Expected: HTTP 400
+     */
+
+    /**
+     * Testing scenario where validator throws {@link com.royalmail.barcode.exception.InvalidSerialNumberException}
+     * Expected: HTTP 400
+     */
+
 }
