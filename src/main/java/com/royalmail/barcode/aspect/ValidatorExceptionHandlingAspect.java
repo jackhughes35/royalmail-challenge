6755aa4b2de857d82cffd6fe57fcd38f5701613d
612750911 <jack.3.hughes@bt.com>
2023-08-11 17:47:17 +0100

    Update exception handling

--- src/main/java/com/royalmail/barcode/aspect/ValidatorExceptionHandlingAspect.java
+++ src/main/java/com/royalmail/barcode/aspect/ValidatorExceptionHandlingAspect.java
@@ -2,6 +2,7 @@ package com.royalmail.barcode.aspect;
 
 import com.royalmail.barcode.exception.InvalidCountryCodeException;
 import com.royalmail.barcode.exception.InvalidPrefixException;
+import com.royalmail.barcode.exception.InvalidSerialNumberException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -17,14 +18,16 @@ import org.springframework.stereotype.Component;
 public class ValidatorExceptionHandlingAspect {
 
     @Around("execution(* com.royalmail.barcode.utilities.BarcodeValidator.validateBarcode(..))  && args(barcode,..)")
-    public Object handelLogging(ProceedingJoinPoint joinPoint, String barcode) throws Throwable {
+    public Object catchCountryCodeExceptions(ProceedingJoinPoint joinPoint, String barcode) throws Throwable {
         // TODO: Should thius be thrown from the specific method?
         try {
             return joinPoint.proceed();
-        }
-        catch (IllegalArgumentException e) {
-        log.info("Message :: {} Incorrect Country code provided {}", e.getLocalizedMessage(), barcode.substring(barcode.length() - 2));
-        throw new InvalidCountryCodeException("Invalid Prefix provided");
+        } catch (NumberFormatException e) {
+            log.info("Message :: {} Incorrect Serial Code format {}", e.getLocalizedMessage(), barcode.substring(barcode.length() - 2));
+            throw new InvalidSerialNumberException("Invalid serial number provided");
+        } catch (IllegalArgumentException e) {
+            log.info("Message :: {} Incorrect Country code provided {}", e.getLocalizedMessage(), barcode.substring(barcode.length() - 2));
+            throw new InvalidCountryCodeException("Invalid CountryCode provided");
         }
     }
 
